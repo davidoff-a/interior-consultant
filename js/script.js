5ac3560458369c9641cb70a94a98a07b62cfbda8
davidoff-a <davidoff.aleksei@gmail.com>
2022-09-09 11:57:53 +0300

    feat: make responsive layout and js work properly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu-nav");
+const nav = document.querySelector(".nav");
+
 burger.addEventListener("click", ({ target }) => {
   if (target) {
+    target.closest(".burger").classList.toggle("twist");
+    if (nav) {
+      nav.classList.toggle("showMenu");
+    }
+  }
+});
+
+menu.addEventListener("click", ({ target }) => {
+  if (target && target.classList.contains("menu-nav__item")) {
+    nav.classList.toggle("showMenu");
     target.closest(".burger").classList.toggle("twist");
   }
 });
